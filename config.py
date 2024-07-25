40c7543db0747a7fc78d9e49fcbffcb174a3b7ee
Mahayushh <ayushmaheshwari6300@gmail.com>
2024-07-25 17:08:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("6966179499:AAGuaip2RzykgKen1qlPd5jyBOPvCMgnSNk")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Mahayush")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1351558132")
 
 
 
